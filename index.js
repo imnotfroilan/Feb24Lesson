978e8ad8e3482a4d5544baa53b344f6751919e73
Karl <Karladrian.solmiano.iics@ust.edu.ph>
2020-02-24 12:31:04 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//1. Using object literal
+
+//int x = 8;
+let person = {name: "mama q", gender: "female", age: "49"}
+
+console.log(person.age);
+console.log(`${person.name} is a ${person.gender} with the age of ${person.age}`);
