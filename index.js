2999e128b2c5b815c364f8ae2920f0f583ebfbec
gggghale <falquezamiguel@gmail.com>
2020-02-24 13:57:58 +0800

    ninth commit

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
+//encapsulation 
 
-//8 STATIC METHOD
-
-class Studentz
+class Student
 {
-    constructor(name, course, section)
+    setName(name)
     {
         this.name = name;
-        this.course = course;
-        this.section = section;
     }
-    
-    enroll(subject)
+
+    getName()
     {
-        console.log(`${this.name} is enrolled in ${subject}`);
+        return this.name;
     }
-    
+
+    setSection(Section)
+    {
+        this.Section = Section;
+    }
+
+    getSection()
+    {
+        return this.Section;
+    }
+
+    setCourse(Course)
+    {
+        this.Course = Course;
+    }
+
+    getCourse()
+    {
+        return this.Course;
+    }
+
+    showInfo()
+    {
+        console.log(`${this.name}, ${this.Section}, ${this.Course}`);
+    }
 }
 
-let unangStudent = new Studentz ("maria alphachupapi", "Nursing", "1Nu2");
-Studentz.enroll("biology");
+let stud = new Student();
+stud.setName ("Ranchodas Chamaldas Chanchan");
+stud.setSection ("1CS");
+stud.setCourse  ("Computer Science");
+
+stud.showInfo();
