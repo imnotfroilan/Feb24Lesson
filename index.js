f4e13727b46c48d141c4a88c0e133095702e3106
Karl <Karladrian.solmiano.iics@ust.edu.ph>
2020-02-24 12:41:18 +0800

    second commit

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ let person = {name: "mama q", gender: "female", age: "49"}
 
 console.log(person.age);
 console.log(`${person.name} is a ${person.gender} with the age of ${person.age}`);
+
+//2. using factory function
+function createPerson(name, gender, age)
+{
+   let p = {};
+   p.name = name;
+   p.gender = gender;
+   p.age = age;
+   return p;
+}
+
+let juan = createPerson("juan dela cruz", "male", "69");
+console.log(`${juan.name} is a ${juan.gender} and is ${juan.age} years old`)
